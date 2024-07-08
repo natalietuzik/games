5c01adcda6953ebfa35c8e390ebcc26c4393b5c8
Natalie T <natalietuzik@gmail.com>
2024-07-08 13:48:18 -0500

    Create database.rb
    coded entry method for database and list code

---
+++ database.rb
@@ -0,0 +1,49 @@
+require_relative "game"
+
+class Database
+  def initialize
+    @games = []
+  end
+
+  def run
+    loop do
+      puts "\n"
+      puts "Welcome to the Games Database!"
+      puts "1. Add a game"
+      puts "2. List all games"
+      puts "3. Exit"
+      print "Choose an option: "
+      option = gets.chomp.to_i
+      puts "\n"
+      case option
+      when 1
+        add_game
+      when 2
+        list_games
+      when 3
+        puts "Thank you for visiting the Games Database. Goodbye!"
+        break
+      else
+        puts "Invalid option. Please try again."
+      end
+    end
+  end
+
+  private
+
+  def add_game
+    print "Enter game title: "
+    title = gets.chomp
+    print "Enter platform name: "
+    platform = gets.chomp
+    @books << Game.new(title, platform)
+    puts "Game added successfully!"
+  end
+
+  def list_games
+    puts "Games in the collection:"
+    @games.each do |game|
+      puts game
+    end
+  end
+end
