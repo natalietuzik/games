5e7aaab7c9c2d1bc5aa365f34123fcdc30fe5133
Natalie T <natalietuzik@gmail.com>
2024-07-11 19:13:39 +0000

    fixed code for tests to solve error

--- tests/tests.rb
+++ tests/tests.rb
@@ -1,19 +1,51 @@
 require 'minitest/autorun'
-require   './app'
+require_relative '../database'
+require_relative '../games'
 
-class MyTest < Minitest::Test
-  def test_user_input
-    # Simulate user input by replacing $stdin with StringIO
-    simulated_user_input = StringIO.new
+class DatabaseTest < Minitest::Test
+  def setup
+    @database = Database.new
+  end
+
+  def test_add_game
+    # Simulate user input
+    simulated_user_input = StringIO.new("Test Game\nTest Platform\n")
     $stdin = simulated_user_input
 
-    # Call the method that we are testing
-    output = Database.run
+    # Call add_game method
+    @database.send(:add_game)
 
-    # Restore $stdin so other tests are not affected by replacement
+    # Restore $stdin
     $stdin = STDIN
 
-    # Assert the expected outcome of the test
-    assert_equal "expected output", output
+    # Verify that the game was added
+    assert_equal 1, @database.instance_variable_get(:@games).size
+    game = @database.instance_variable_get(:@games).first
+    assert_equal "Test Game", game.title
+    assert_equal "Test Platform", game.platform
+  end
+
+  def test_list_games
+    # Add a game to the database
+    game = Games.new("Test Game", "Test Platform")
+    @database.instance_variable_get(:@games) << game
+
+    # Capture the output of the list_games method
+    output = capture_io { @database.send(:list_games) }.first
+
+    # Verify the output
+    assert_match /Test Game for Test Platform/, output
+  end
+
+  private
+
+  # Helper method to capture output
+  def capture_io
+    out, err = StringIO.new, StringIO.new
+    $stdout, $stderr = out, err
+    yield
+    [out.string, err.string]
+  ensure
+    $stdout, $stderr = STDOUT, STDERR
   end
 end
